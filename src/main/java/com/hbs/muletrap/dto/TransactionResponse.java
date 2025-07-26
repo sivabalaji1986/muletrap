e7c9a65da03c94556d8fe9e0b6029a4d6ebdac73
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-07-26 14:19:16 +0800

    Created a new DTO for response - TransactionResponse

---
+++ src/main/java/com/hbs/muletrap/dto/TransactionResponse.java
@@ -0,0 +1,19 @@
+package com.hbs.muletrap.dto;
+
+import lombok.Data;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+public class TransactionResponse {
+    private UUID id;
+    private BigDecimal amount;
+    private String channel;
+    private String time;
+    private String country;
+    private int accountAgeDays;
+    private String activitySummary;
+    private boolean mule;
+    private LocalDateTime createdAt;
+}
