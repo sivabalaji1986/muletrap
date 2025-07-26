d6de113cc3fc5e266d77cfcd9df025ba80ccf1a0
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-07-26 08:14:06 +0800

    Created Tests class

---
+++ src/test/java/com/hbs/muletrap/MuletrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbs.muletrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuletrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
