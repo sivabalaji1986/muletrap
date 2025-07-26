3f3a4254be2f9d9c0aa8f9d657727ab9141fe642
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-07-26 08:13:56 +0800

    Created Main Class

---
+++ src/main/java/com/hbs/muletrap/MuleTrapApplication.java
@@ -0,0 +1,11 @@
+package com.hbs.muletrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuleTrapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MuleTrapApplication.class, args);
+    }
+}
