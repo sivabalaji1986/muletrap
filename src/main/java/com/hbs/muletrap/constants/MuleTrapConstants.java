672a52a79d9c206f71f48d7a141f3725f6d48794
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-07-26 09:32:13 +0800

    Created MuleTrapConstants and updated New Account Days to 90

---
+++ src/main/java/com/hbs/muletrap/constants/MuleTrapConstants.java
@@ -0,0 +1,19 @@
+package com.hbs.muletrap.constants;
+
+public class MuleTrapConstants {
+
+    public static final String HIGH_RISK_LABEL       = "high‑risk";
+
+    public static final String LOW_RISK_LABEL        = "low‑risk";
+
+    public static final String HIGH_VALUE_LABEL      = "high‑value";
+
+    public static final String MEDIUM_VALUE_LABEL    = "medium";
+
+    public static final String LOW_VALUE_LABEL       = "low";
+
+    public static final String NEWLY_OPENED_LABEL    = "newly opened";
+
+    public static final String ESTABLISHED_LABEL     = "established";
+
+}
