47abcbfe47a155d2265f27ea5a0552e534ece6a3
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-07-26 11:27:45 +0800

    Updated TransactionInput

--- src/main/java/com/hbs/muletrap/dto/TransactionInput.java
+++ src/main/java/com/hbs/muletrap/dto/TransactionInput.java
@@ -1,14 +1,27 @@
 package com.hbs.muletrap.dto;
 
 import lombok.Data;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
 public class TransactionInput {
+    @NotNull
     private BigDecimal amount;
+
+    @NotBlank
     private String channel;
+
+    @NotBlank
     private String time;
+
+    @NotBlank
     private String country;
+
+    @Min(0)
     private int accountAgeDays;
+
     private String activitySummary;
 }
