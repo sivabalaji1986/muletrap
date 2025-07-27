2caf533941352d61cd23f0602b868b16dd1feb53
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-07-27 16:43:42 +0800

    Explicitly loaded config

--- src/main/java/com/hbs/muletrap/MuleTrapApplication.java
+++ src/main/java/com/hbs/muletrap/MuleTrapApplication.java
@@ -1,9 +1,12 @@
 package com.hbs.muletrap;
 
+import com.hbs.muletrap.config.FraudConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(FraudConfig.class)
 public class MuleTrapApplication {
     public static void main(String[] args) {
         SpringApplication.run(MuleTrapApplication.class, args);
